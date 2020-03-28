8d9112e15617096d36d84ac0d906ec1849f0eacf
jmgarridopaz <44113936+jmgarridopaz@users.noreply.github.com>
2020-03-28 10:08:10 +0100

    Create commentboxio.js

---
+++ assets/js/commentboxio.js
@@ -0,0 +1,7 @@
+import commentBox from 'commentbox.io';
+// or
+//const commentBox = require('commentbox.io');
+// or if using the CDN, it will be available as a global "commentBox" variable.
+
+// id of my commentbox.io project
+commentBox('5650367689785344-proj');
