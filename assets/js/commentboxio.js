306a98c4c24ac8712be706c600653cc8595fd7b8
jmgarridopaz <44113936+jmgarridopaz@users.noreply.github.com>
2020-03-28 19:08:07 +0100

    Update commentboxio.js

--- assets/js/commentboxio.js
+++ assets/js/commentboxio.js
@@ -1,6 +1,6 @@
-const commentBox = require('commentbox.io');
+import commentBox from 'commentbox.io';
 // or
-//import commentBox from 'commentbox.io';
+//const commentBox = require('commentbox.io');
 // or if using the CDN, it will be available as a global "commentBox" variable.
 
 // id of my commentbox.io project
