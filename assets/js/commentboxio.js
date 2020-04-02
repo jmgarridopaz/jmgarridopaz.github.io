23e36fa6c1e0507f1eec23be297be4f21afb907f
jmgarridopaz <44113936+jmgarridopaz@users.noreply.github.com>
2020-04-02 14:31:15 +0200

    Delete commentboxio.js

--- assets/js/commentboxio.js
+++
@@ -1,7 +0,0 @@
-import commentBox from 'commentbox.io';
-// or
-// const commentBox = require('commentbox.io');
-// or if using the CDN, it will be available as a global "commentBox" variable.
-
-// my project id
-commentBox('5650367689785344-proj');
